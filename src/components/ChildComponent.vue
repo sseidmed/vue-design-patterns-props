3ec365c99d6cc032097b6fcd0104b76e2563c813
Shay Seidmedova <shahloseidmedova@Shays-MBP.attlocal.net>
2022-06-12 00:07:21 -0400

    Add vitest and 2 components

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h3>Child</h3>
+    <p>{{ props.message }}</p>
+    <hr />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: false,
+    default: "I am the default message",
+    validator: validateMessage
+  },
+});
+
+
+</script>
+
+<script>
+
+const validateMessage = (message) => {
+    if (!['I am the default message', 'Clean your room', 'Take out the trash', 'Find the remote'].includes(message)) {
+        throw Error(`Message not identified. You provided: ${message}.`)
+    }
+}
+export default {}
+</script>
+
+
+<style scoped>
+.container {
+  margin: 20px;
+  border: 1px solid black;
+  padding: 10px;
+  width: 50%;
+}
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h3>Parent</h3>
+
+    <ChildComponent
+      message="Clean your room"
+    />
+  </div>
+</template>
+
+<script setup>
+import ChildComponent from "./ChildComponent.vue";
+
+</script>
+
+<style scoped>
+.container {
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
